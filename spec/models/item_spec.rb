61f509650ef55493571ec49dadfa72c4b30e52ec
yryuz <81128880+yryuz@users.noreply.github.com>
2021-04-19 15:46:03 +0900

    itemモデルテストコード追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -78,10 +78,22 @@ RSpec.describe Item, type: :model do
     expect(@item.errors.full_messages).to include("Category can't be blank", "Category is not a number")
   end
 
+  it "カテゴリーのidが１の場合出品できない" do
+    @item.category_id = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Category must be other than 1")
+  end
+
   it "商品の状態の情報がない場合出品できない" do
     @item.condition_id = ""
     @item.valid?
     expect(@item.errors.full_messages).to include("Condition can't be blank", "Condition is not a number")
+  end
+
+  it "商品状態のidが１の場合出品できない" do
+    @item.condition_id = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Condition must be other than 1")
   end
 
   it "配送料の負担についての情報がない場合出品できない" do
@@ -90,16 +102,34 @@ RSpec.describe Item, type: :model do
     expect(@item.errors.full_messages).to include("Postage payer can't be blank", "Postage payer is not a number")
   end
 
+  it "配送料の負担についてのidが１の場合出品できない" do
+    @item.postage_payer_id = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Postage payer must be other than 1")
+  end
+
   it "発送元の地域の情報がない場合出品できない" do
     @item.prefecture_id = ""
     @item.valid?
     expect(@item.errors.full_messages).to include("Prefecture can't be blank", "Prefecture is not a number")
+  end
+
+  it "発送元の地域の情報のidが１の場合出品できない" do
+    @item.prefecture_id = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
   end
     
  it "発送までの日数の情報がない場合出品できない" do
   @item.preparation_day_id = ""
   @item.valid?
   expect(@item.errors.full_messages).to include("Preparation day can't be blank", "Preparation day is not a number")
+  end
+
+  it "発送までの日数のidが１の場合出品できない" do
+    @item.preparation_day_id = 1
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Preparation day must be other than 1")
   end
 
 end
