b06831510fcd97d8e95ca3d4a7cd453992e3de8f
yryuz <81128880+yryuz@users.noreply.github.com>
2021-04-19 16:12:56 +0900

    コメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    @items = Item.includes(:user)
+   ## @items = Item.includes(:user)
   end
 
   def new
